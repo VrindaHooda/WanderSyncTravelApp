2183c7eac749ecfa364011f59822376f277e5f1f
vrindahooda <vrindahooda@gmail.com>
2024-10-02 18:17:48 -0400

    Create travel community activity file.

---
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/TravelCommunityActivity.java
@@ -0,0 +1,14 @@
+package com.example.sprintproject.views;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.sprintproject.R;
+
+public class TravelCommunityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.travel_community_screen); // Load the travel community screen layout
+    }
+}
