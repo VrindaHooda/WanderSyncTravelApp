8d18945db4e2b960843dc9c42f99a3a526fadf75
vrindahooda <vrindahooda@gmail.com>
2024-10-02 18:16:56 -0400

    Create transportation activity file.

---
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/TransportationActivity.java
@@ -0,0 +1,14 @@
+package com.example.sprintproject.views;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.sprintproject.R;
+
+public class TransportationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.transportation_screen); // Load the transportation screen layout
+    }
+}
