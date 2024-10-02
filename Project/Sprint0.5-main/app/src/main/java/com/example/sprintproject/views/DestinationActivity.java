b005978f465847020fe55e32c48b180953805496
vrindahooda <vrindahooda@gmail.com>
2024-10-02 18:13:42 -0400

    Create destination activity file.

---
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/DestinationActivity.java
@@ -0,0 +1,14 @@
+package com.example.sprintproject.views;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.sprintproject.R;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.destination_screen); // Load the destination screen layout
+    }
+}
