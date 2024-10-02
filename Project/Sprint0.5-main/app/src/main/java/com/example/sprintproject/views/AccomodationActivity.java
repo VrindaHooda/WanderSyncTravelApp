9b166777e4bac83ba7b6fec6c990a66e9b6e1540
vrindahooda <vrindahooda@gmail.com>
2024-10-02 18:16:13 -0400

    Create accomodation activity file.

---
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/AccomodationActivity.java
@@ -0,0 +1,14 @@
+package com.example.sprintproject.views;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.sprintproject.R;
+
+public class AccomodationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accomodation_screen); // Load the accommodation screen layout
+    }
+}
