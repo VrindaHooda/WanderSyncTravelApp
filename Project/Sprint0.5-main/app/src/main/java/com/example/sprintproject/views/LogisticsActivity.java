66bf77f58294bb46fdde8469a7da6c8e01bc9a70
vrindahooda <vrindahooda@gmail.com>
2024-10-02 18:11:55 -0400

    Create logistics activity file.

---
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/LogisticsActivity.java
@@ -0,0 +1,15 @@
+package com.example.sprintproject.views;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sprintproject.R;
+
+public class LogisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logistics_screen); // Load the logistics screen layout
+    }
+}
