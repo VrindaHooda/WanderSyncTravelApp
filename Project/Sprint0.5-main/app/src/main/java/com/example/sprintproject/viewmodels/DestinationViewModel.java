cf317eaf22b95de0247429473c888114dc3e3dda
Aditi Sharma <aditi01@gatech.edu>
2024-10-24 21:35:38 -0400

    Fix errors

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/viewmodels/DestinationViewModel.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/viewmodels/DestinationViewModel.java
@@ -5,9 +5,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import com.example.sprintproject.model.DestinationDatabase;
 import com.example.sprintproject.model.DestinationEntry;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.Date;
 import java.util.List;
@@ -18,33 +15,27 @@ public class DestinationViewModel extends ViewModel {
     private MutableLiveData<List<DestinationEntry>> destinationEntriesLiveData;
 
     public DestinationViewModel() {
-        // Get singleton instance of DestinationDatabase
         destinationDatabase = DestinationDatabase.getInstance();
         destinationEntriesLiveData = new MutableLiveData<>();
     }
 
-    // Method to generate a unique destination ID
     public String generateDestinationId(String location, Date startDate) {
         return location + "_" + startDate.getTime();
     }
 
-    // Add destination entry to Firebase using destinationId
     public void addDestination(DestinationEntry entry) {
         destinationDatabase.addEntry(entry.getDestinationId(), entry);
     }
 
-    // Read all destination entries from Firebase
     public void readEntries() {
         destinationDatabase.getAllEntries(new DestinationDatabase.DataStatus() {
             @Override
             public void DataIsLoaded(List<DestinationEntry> entries) {
-                // Update LiveData when data is loaded from Firebase
                 destinationEntriesLiveData.setValue(entries);
             }
         });
     }
 
-    // Return the LiveData to observe in the UI
     public LiveData<List<DestinationEntry>> getDestinationEntries() {
         return destinationEntriesLiveData;
     }
