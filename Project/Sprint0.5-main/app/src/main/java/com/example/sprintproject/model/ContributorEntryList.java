332647b6f140850bd66c8462c270595a1fafcf5a
tanyakhurana <cooltanyak@gmail.com>
2024-11-10 15:43:03 -0500

    Button for add notes

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/model/ContributorEntryList.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/model/ContributorEntryList.java
@@ -7,12 +7,18 @@ public class ContributorEntryList {
     private List<ContributorEntry> contributorEntryList;
 
     public ContributorEntryList() {
-
+        this.contributorEntryList = new ArrayList<>();
     }
 
     public ContributorEntryList(List<ContributorEntry> contributorEntryList) {
         this.contributorEntryList = contributorEntryList;
     }
+
+    public void add(ContributorEntry entry) {
+        contributorEntryList.add(entry);
+
+    }
+
 
     public void setContributorEntryList(List<ContributorEntry> contributorEntryList) {
         this.contributorEntryList = contributorEntryList;
