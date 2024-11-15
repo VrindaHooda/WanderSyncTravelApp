e495c16f7534ce6bd919a8c75ee4ffbe3bc33834
Afsheen Mohammad <amohammad38@gatech.edu>
2024-11-14 22:48:18 -0500

    vrindamonkey

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/DiningReservationAdapter.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/DiningReservationAdapter.java
@@ -1,12 +1,18 @@
 package com.example.sprintproject.views;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.sprintproject.databinding.ReservationListItemBinding;
 import com.example.sprintproject.model.DiningReservation;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class DiningReservationAdapter
         extends RecyclerView.Adapter<DiningReservationAdapter.ReservationViewHolder> {
@@ -29,6 +35,35 @@ public class DiningReservationAdapter
     @Override
     public void onBindViewHolder(@NonNull ReservationViewHolder holder, int position) {
         DiningReservation reservation = reservationList.get(position);
+
+        // Get current date and time
+        Calendar today = Calendar.getInstance();
+
+        // Set reservation date and time
+        Calendar reservationDate = Calendar.getInstance();
+        reservationDate.set(reservation.getYear(), reservation.getMonth() - 1, reservation.getDay());
+
+        // Parse time and set it in the reservation calendar
+        try {
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+            Date reservationTime = timeFormat.parse(reservation.getTime());
+            if (reservationTime != null) {
+                Calendar timeCalendar = Calendar.getInstance();
+                timeCalendar.setTime(reservationTime);
+                reservationDate.set(Calendar.HOUR_OF_DAY, timeCalendar.get(Calendar.HOUR_OF_DAY));
+                reservationDate.set(Calendar.MINUTE, timeCalendar.get(Calendar.MINUTE));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Check if the reservation date and time are in the past
+        if (reservationDate.before(today)) {
+            holder.binding.getRoot().setBackgroundColor(Color.GRAY); // Expired reservation
+        } else {
+            holder.binding.getRoot().setBackgroundColor(Color.GREEN); // Upcoming reservation
+        }
+
         holder.bind(reservation);
     }
 
